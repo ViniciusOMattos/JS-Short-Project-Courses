7fd6eb811afafd5e78d506a2c2d2248b46e78bee
ViniciusOMattos <83370086+ViniciusOMattos@users.noreply.github.com>
2022-03-23 11:29:56 -0300

    JS Ajax Test

---
+++ PokeGen JS/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+    $('#get-poke-button').on('click', function() {
+
+        $.get('https://pokeapi.co/api/v2/pokemon/', function(data, status){
+            console.log(data);
+
+            var string = "";
+            string += "<table>";
+
+            $.each(data.results, function(index, item){
+                string += "<tr><td><a href='"+item.url+"'>"+item.name+"</td><td><button class='cath-button btn btn-primary'>Cath</button></td></tr>";
+            })
+
+            string += "</table>";
+
+            $('#poke-list').html(string);
+        })
+
+    });
+
+});

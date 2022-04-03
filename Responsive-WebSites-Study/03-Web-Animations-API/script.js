46e17edc629f9b9f794daced1bebb7d2035e2a4f
ViniciusOMattos <83370086+ViniciusOMattos@users.noreply.github.com>
2022-04-03 11:56:23 -0300

    left/right/reset positions anim

--- Responsive-WebSites-Study/03-Web-Animations-API/script.js
+++ Responsive-WebSites-Study/03-Web-Animations-API/script.js
@@ -1,20 +1,37 @@
 const box = document.querySelector(".ani-box")
 
-const btnPrev = document.getElementById("btn-prev");
-const btnNext = document.getElementById("btn-next");
+const btnReset = document.getElementById("btn-reset");
+const btnLeft = document.getElementById("btn-left");
+const btnRight = document.getElementById("btn-right");
 
-btnNext.addEventListener("click", nextAni);
-btnPrev.addEventListener("click", prevAni);
+btnLeft.addEventListener("click", leftAni);
+btnReset.addEventListener("click", resetAni);
+btnRight.addEventListener("click", rightAni);
 
 let currentId = 0;
 
-function nextAni() {
-    if (currentId == 0) {
+function resetAni() {
+    if (currentId == 1) {
         box.animate([
             {
-                transform: "translate(200px, 30px)",
+                transform: "translate(360px, 200px)",
                 backgroundColor: "#f95",
                 borderRadius: "0px",
+            }], {
+                delay:500,
+                fill: "both",
+                easing: "ease-in-out",
+                duration: 1000,
+            })
+        currentId = 0;
+    }
+}
+
+function leftAni() {
+    if (currentId == 0 || currentId == 1) {
+        box.animate([
+            {
+                transform: "translate(150px, 200px)",
             }], {
                 delay:500,
                 fill: "both",
@@ -25,19 +42,17 @@ function nextAni() {
     }
 }
 
-function prevAni() {
-    if (currentId == 1) {
+function rightAni() {
+    if (currentId == 0 || currentId == 1) {
         box.animate([
             {
-                transform: "translate(0, 200px)",
-                backgroundColor: "#f95",
-                borderRadius: "0px",
+                transform: "translate(600px, 200px)",
             }], {
                 delay:500,
                 fill: "both",
                 easing: "ease-in-out",
                 duration: 1000,
             })
-        currentId = 0;
+        currentId = 1;
     }
 }
